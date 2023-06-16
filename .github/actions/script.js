b3fae06392920c1050c05c25d17dd5de0894d10a
Babatunde Aromire <babatunde.aromire@cognite.com>
2023-06-16 16:37:04 +0200

    all commands

--- .github/actions/script.js
+++ .github/actions/script.js
@@ -41,81 +41,28 @@ module.exports = {
     return command.trim()
   },
   sync: function(core, manifestPath, noVerify) {
-    let sync_command = 'publish-extractor sync '
+    let command = 'publish-extractor sync '
     if (manifestPath.trim() == '') {
       core.error("Path to manifest file is required")
       process.exit(1)
     }
-    sync_command += '--manifest ' + manifestPath + ' '
-    sync_command += '--no-verify ' + bool_check(core, 'no-verify', noVerify) + ' '
-    return sync_command.trim()
-  }
+    command += '--manifest ' + manifestPath + ' '
+    command += '--no-verify ' + bool_check(core, 'no-verify', noVerify) + ' '
+    return command.trim()
+  },
+  validate: function(core, manifestPath, configFiles) {
+    let command = 'publish-extractor validate '
+    if (manifestPath.trim() == '') {
+      core.error("Path to manifest file is required")
+      process.exit(1)
+    }
+
+    command += '--manifest ' + manifestPath + ' '
+    if ('${{ inputs.config-files }}'.trim() !== '') {
+      command += '--config-files ' + configFiles + ' '
+    }
+    return command
+  },
+  runSchema: function(core) {},
+  schema: function (core) {}
 }
-
-
-
-
-// if ('${{ inputs.command }}'.trim() == '') {
-//     core.exit('command is required')
-//     process.exit(1)
-//   }
-
-//   function bool_check(prop, value) {
-//     if (['true', 'yes', 'false', 'no', ''].indexOf(value) === -1) {
-//       core.error('Invalid value passed for ' + prop + ', boolean expected');
-//       process.exit(1)
-//     }
-//     return ['true', 'yes'].indexOf(value) ? 'true' : 'false'
-//   }
-
-//   switch('${{ inputs.command }}') {
-//     case 'publish':
-//       let command = 'publish-extractor publish '
-//       if ('${{ inputs.version }}'.trim() == '') {
-//         core.error("Version is required in semver format")
-//         process.exit(1)
-//       }
-//       command += '--version ${{ inputs.version }} '
-
-//       if ('${{ inputs.manifest_path }}'.trim() == '') {
-//         core.error("Path to manifest file is required")
-//         process.exit(1)
-//       }
-//       command += '--manifest ${{ inputs.manifest_path }} '
-
-//       command += '--no-verify ' + bool_check('no-verify', ${{ inputs.no-verify }}) + ' '
-
-//       command += '--only-artifacts ' + bool_check('no-verify', ${{ inputs.no-verify }}) + ' '
-//       return command.trim()
-
-//     case 'sync':
-//       let sync_command = 'publish-extractor sync '
-//       if ('${{ inputs.manifest_path }}'.trim() == '') {
-//         core.error("Path to manifest file is required")
-//         process.exit(1)
-//       }
-//       command += '--manifest ${{ inputs.manifest_path }} '
-//       command += '--no-verify ' + bool_check('no-verify', ${{ inputs.no-verify }}) + ' '
-//       return command.trim()
-
-//     case 'validate':
-//       let validate_command = 'publish-extractor validate '
-//       if ('${{ inputs.manifest_path }}'.trim() == '') {
-//         core.error("Path to manifest file is required")
-//         process.exit(1)
-//       }
-
-//       command += '--manifest ${{ inputs.manifest_path }} '
-//       if ('${{ inputs.config-files }}'.trim() !== '') {
-//         let files = '${{ inputs.config-files }}'.trim().split(',')
-//         command += '--config-files ${{ inputs.config-files }} '
-//       }
-//       return command
-//     case 'run-schema':
-//       break
-//     case 'schema': 
-//       break
-//     default:
-//       core.error('unrecognized command passed')
-//     process.exit(1)
-//   }
